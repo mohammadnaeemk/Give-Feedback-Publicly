1220188e278944cfe340738dd6edec15421ce77f
Naeem <naeemkarimzadeh@gmail.com>
2024-09-18 21:26:05 +0330

    The mini project is almost finished

--- javascript.js
+++ javascript.js
@@ -5,10 +5,14 @@ const ListEL = document.querySelector(".hashtags");
 const formEL = document.querySelector(".form");
 const feedbacksLE = document.querySelector(".feedbacks");
 const spinnerLE = document.querySelector(".spinner");
+const hashtagListEL = document.querySelector('.hashtags');
 counterEl.textContent = 150;
 let matn = "#";
 let Flag = false;
+let list;
+let listNumber;
 
+const URLlink = "https://bytegrad.com/course-assets/js/1/api/feedbacks";
 textAreaEL.addEventListener("input", function () {
   counterEl.textContent = 150 - textAreaEL.value.length;
 });
@@ -79,16 +83,135 @@ const SubmitHandler = (item) => {
           <p class="feedback__date">${time}</p>
       </li>
   `;
+ 
   if (Flag == true) {
     feedbacksLE.insertAdjacentHTML("beforeend", feedHTML);
     textAreaEL.value = "";
     counterEl.textContent = 150;
-    spinnerLE.style="display : none;"
+    spinnerLE.remove();
   } else {
     return;
   }
+  let ob = {
+    upvoteCount : vote ,
+    daysAgo : time ,
+    company: companyName,
+    badgeLetter : companyLetter,
+    text : txt
+  }
+  fetch(URLlink, {
+    method: 'POST',
+    body: JSON.stringify(ob),
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+}).then(response => {
+    if (!response.ok) {
+        console.log('Something went wrong' + response);
+        return;
+    }
+    console.log('Successfuly submitted');
+}).catch(error => console.log(error));
 };
+const printer = function () {
+  for (let index = 0; index < listNumber; index++) {
+    Company = list[index].company;
+    Letter = list[index].badgeLetter;
+    Vote = list[index].upvoteCount;
+    Days = list[index].daysAgo;
+    Text = list[index].text;
 
+    feedHTML = `
+    <li class="feedback">
+            <button class="upvote">
+                <i class="fa-solid fa-caret-up upvote__icon"></i>
+                <span class="upvote__count">${Vote}</span>
+            </button>
+            <section class="feedback__badge">
+                <p class="feedback__letter">${Letter}</p>
+            </section>
+            <div class="feedback__content">
+                <p class="feedback__company">${Company}</p>
+                <p class="feedback__text">${Text}</p>
+            </div>
+            <p class="feedback__date">${Days}</p>
+        </li>
+    `;
+    
+    feedbacksLE.insertAdjacentHTML("beforeend", feedHTML);
+  }
+};
 submitbtnEL.addEventListener("click", submitBtnHandler);
 
 formEL.addEventListener("submit", SubmitHandler);
+
+//vars
+let Company, Letter, Vote, Days, Text;
+let feedHTML;
+fetch(URLlink)
+  .then((info) => {
+    return info.json();
+    
+  })
+  .then((data) => {
+    list = data.feedbacks;
+    listNumber = list.length;
+    printer();
+    
+      if (document.querySelector(".feedbacks").getElementsByTagName("li").length > 0) {
+        spinnerLE.remove();
+        
+      }
+    ;
+  
+    
+  }).catch(error => {
+    alert("error cod is : " + error.message)
+  });
+
+
+  const click = event => {
+
+    const clickedEL = event.target;
+
+    const upvoteEL = clickedEL.className.includes('upvote');
+    if (upvoteEL) {
+       const upvoteBtnEl = clickedEL.closest('.upvote');
+       upvoteBtnEl.disabled=true;
+
+       const upvoteCountEl = upvoteBtnEl.querySelector('.upvote__count');
+       let upvoteCount = +upvoteBtnEl.textContent;
+
+       upvoteCountEl.textContent = ++upvoteCount;
+    }
+    else {
+       clickedEL.closest('.feedback').classList.toggle('feedback--expand');
+    }
+};
+
+feedbacksLE.addEventListener('click', click);
+
+
+
+const hashtagClickHandler = event => {
+  const clickedEl = event.target;
+  
+  if (clickedEl.className === 'hashtags') return;
+
+  const companyNameFromHastag = clickedEl.textContent.substring(1).trim();
+ 
+  feedbacksLE.childNodes.forEach(childNode => {
+      if (childNode.nodeType  === 3 ) return;
+      const companyNameFromFeedbackItem = childNode.
+          querySelector('.feedback__company')
+          .textContent.toLowerCase().trim();
+
+      if(companyNameFromHastag.toLowerCase().trim() !== companyNameFromFeedbackItem)
+          {
+              childNode.remove();
+          }
+  });
+
+};
+hashtagListEL.addEventListener('click', hashtagClickHandler);
